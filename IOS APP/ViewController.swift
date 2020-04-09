ea2bc45284e42ab6b37e05a17596c99de9e2ca25
imastudiojkt <info@imastudio.co.id>
2020-04-09 17:14:29 +0700

    Initial Commit

---
+++ IOS APP/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IOS APP
+//
+//  Created by iswandisaputra on 09/04/2020.
+//  Copyright © 2020 iswandisaputra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
